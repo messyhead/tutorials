f3b722b574ed7030f7924506c7330c4fc0d58d0f
anthonym <anthony.mcelroy@ros.gov.uk>
2016-12-23 19:06:37 +0000

    After lesson 66

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -71,24 +71,12 @@ $(document).ready(function() {
         var icon = $('.js--nav-icon i');
         
         nav.slideToggle(200);
-        if (icon.hasClass('.ion-navicon-round')) {
-            icon.addClass('.ion-close-round');
-            icon.removeClass('.ion-navicon-round');
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
         } else {
-            icon.addClass('.ion-navicon-round');
-            icon.removeClass('.ion-close-round');
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
         }
-        
-         
-        
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
 });
