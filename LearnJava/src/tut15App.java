ac3fd345de424e3bde496dae965d20f428e4f69a
Tony McElroy <Tony McElroy>
2017-01-30 20:40:12 +0000

    up to tutorial 17

---
+++ LearnJava/src/tut15App.java
@@ -0,0 +1,38 @@
+//class Person {
+//    String name;
+//    int age;
+//
+//    int calculateYearsToRetirement() {
+//        int yearsLeft = 65 - age;
+//
+//        return yearsLeft;
+//    }
+//
+//    int getAge() {
+//        return age;
+//    }
+//
+//    String getName() {
+//        return name;
+//    }
+//}
+//
+//
+//public class tut15App {
+//    public static void main(String[] args) {
+//        Person person1 = new Person();
+//
+//        person1.name = "Joe";
+//        person1.age = 25;
+//
+//        int years = person1.calculateYearsToRetirement();
+//
+//        System.out.println("Years till retirement " + years);
+//
+//        int age = person1.getAge();
+//        String name = person1.getName();
+//
+//        System.out.println("My name is " + name);
+//        System.out.println("My age is " + age);
+//    }
+//}
